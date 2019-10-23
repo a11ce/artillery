37d0adc28bc785d4f23fb7b45b31111af26cb4c1
a11ce <a11ce@protonmail.com>
2019-10-23 14:25:50 -0400

    AI additions

--- ai.py
+++ ai.py
@@ -1,8 +1,19 @@
 import random
+import numpy as np
 
+def randomNormal(pName, facingLeft):
+    power = np.random.normal(loc=60,scale=10)
+    angle = np.random.normal(loc=45,scale=10)
+    if facingLeft:
+        angle = 180 - angle
+    return (power,angle)
+
+def peaceBot(pName, facingLeft):
+    return (100,90)
+    
 def random45(pName, facingLeft):
-    power = 45
-    angle = random.randint(50,100)
+    power = random.uniform(50,70)
+    angle = 45
     if facingLeft:
         angle = 180 - angle
     return (power,angle)
